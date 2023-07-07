20d527399537d14022f3059fb858741f78566773
Vivatsathorn Thitasirivit <neil4884@gmail.com>
2023-07-07 10:57:14 +0700

    1.1.1

--- include/smart_delay.h
+++ include/smart_delay.h
@@ -16,13 +16,49 @@ namespace vt {
         uint32_t previous_time;
 
     public:
-        explicit smart_delay(uint32_t interval, time_func func)
+        /**
+         * The constructor
+         *
+         * @param interval
+         * @param func
+         */
+        smart_delay(uint32_t interval, time_func func)
                 : func{func}, interval{interval} { previous_time = func(); }
 
+        /**
+         * Default copy constructor
+         */
+        smart_delay(const smart_delay &) = default;
+
+        /**
+         * Disabled move constructor
+         */
+        smart_delay(smart_delay &&) noexcept = delete;
+
+        /**
+         * Conversion to bool returns has_passed().
+         *
+         * @return
+         */
         explicit operator bool() { return has_passed(); }
 
+        /**
+         * Conversion to uint32_t returns interval.
+         *
+         * @return
+         */
         explicit operator uint32_t() const { return interval; }
 
+        /**
+         * Reset timer's interval.
+         */
+        void reset() { previous_time = func(); }
+
+        /**
+         * Check if the interval has passed.
+         *
+         * @return
+         */
         bool has_passed() {
             uint32_t curr_time = func();
             if (curr_time - previous_time >= interval) {
@@ -32,8 +68,18 @@ namespace vt {
             return false;
         }
 
+        /**
+         * Set a new interval.
+         *
+         * @param new_interval
+         */
         void set_interval(uint32_t new_interval) { interval = new_interval; }
 
+        /**
+         * Set time function, i.e., millis or micros.
+         *
+         * @param new_func
+         */
         void set_time_func(time_func new_func) { func = new_func; }
     };
 }
